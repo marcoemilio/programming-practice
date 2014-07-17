d4be26e18c792c59d3925ed7d39140742513952f
Marco Emílio <xtutiz@gmail.com>
2014-07-16 22:22:08 -0300

    initial commit, add stack.c

---
+++ stack.c
@@ -0,0 +1,15 @@
+/*!
+*	@author Marco Emílio
+*
+*	@brief Implementation of a general stack.
+*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Nada\n");
+	return 0;
+}
